0d167be1b309e54a22e3cbbbabf96ebc6a819d2c
JoseAlvarezP7 <josealvarez7p@gmail.com>
2021-05-27 17:47:36 -0500

    Logic

--- lib/main.dart
+++ lib/main.dart
@@ -19,14 +19,15 @@ class _MyAppState extends State<MyApp> {
   );
 
   double _numberFrom;
+  String _startMeasure;
+  String _convertedMeasure;
+  String _resultMessage;
 
   void initState() {
     _numberFrom = 0;
     // _startMeasure = 'miles';
     super.initState();
   }
-
-  var fruits = ['Orange', 'Apple', 'Strawberry', 'Banana'];
 
   final List<String> _measures = [
     'meters',
@@ -38,8 +39,44 @@ class _MyAppState extends State<MyApp> {
     'pounds (lbs)',
     'pounces',
   ];
-  String _startMeasure;
-  String _convertedMeasure;
+
+  final Map<String, int> _measuresMap = {
+    'meters': 0,
+    'kilometers': 1,
+    'grams': 2,
+    'kilograms': 3,
+    'feet': 4,
+    'miles': 5,
+    'pounds (lbs)': 6,
+    'pounces': 7,
+  };
+
+  final dynamic _formulas = {
+    '0': [1, 0.001, 0, 0, 3.28084, 0.000621371, 0, 0],
+    '1': [1000, 1, 0, 0, 3280.84, 0.621371, 0, 0],
+    '2': [0, 0, 1, 0.0001, 0, 0, 0.00220462, 0.035274],
+    '3': [0, 0, 1000, 1, 0, 0, 2.20462, 35.274],
+    '4': [0.3048, 0.0003048, 0, 0, 1, 0.000189394, 0, 0],
+    '5': [1609.34, 1.60934, 0, 0, 5280, 1, 0, 0],
+    '6': [0, 0, 453.592, 0.453592, 0, 0, 1, 16],
+    '7': [0, 0, 28.3495, 0.0283495, 3.28084, 0, 0.0625, 1],
+  };
+
+  void convert(double value, String from, String to) {
+    int nFrom = _measuresMap[from];
+    int nTo = _measuresMap[to];
+    var multiplier = _formulas[nFrom.toString()][nTo];
+    var result = value * multiplier;
+    if (result == 0) {
+      _resultMessage = 'This conversion cannot be performed';
+    } else {
+      _resultMessage =
+          '${_numberFrom.toString()} $_startMeasure are ${result.toString()} $_convertedMeasure';
+    }
+    setState(() {
+      _resultMessage = _resultMessage;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,13 +90,75 @@ class _MyAppState extends State<MyApp> {
           padding: EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             children: [
-              Spacer(),
-              Text(
-                'Value',
-                style: labelStyle,
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'From',
+                        style: labelStyle,
+                      ),
+                      DropdownButton<String>(
+                        hint: Text(
+                          "Unit",
+                          style: inputStyle,
+                        ),
+                        value: _startMeasure,
+                        items: _measures.map((String _measuresValue) {
+                          return DropdownMenuItem(
+                            value: _measuresValue,
+                            child: Text(
+                              _measuresValue,
+                              style: inputStyle,
+                            ),
+                          );
+                        }).toList(),
+                        onChanged: (String newValue) {
+                          setState(() {
+                            _startMeasure = newValue;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'To',
+                        style: labelStyle,
+                      ),
+                      DropdownButton(
+                        hint: Text(
+                          "Unit",
+                          style: inputStyle,
+                        ),
+                        items: _measures.map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(
+                              value,
+                              style: inputStyle,
+                            ),
+                          );
+                        }).toList(),
+                        onChanged: (value) {
+                          setState(() {
+                            _convertedMeasure = value;
+                          });
+                        },
+                        value: _convertedMeasure,
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              Spacer(),
+              SizedBox(height: 30),
               TextField(
+                keyboardType: TextInputType.number,
                 style: inputStyle,
                 decoration: InputDecoration(
                   hintText: "Please insert the measure to be converted",
@@ -73,65 +172,27 @@ class _MyAppState extends State<MyApp> {
                   }
                 },
               ),
-              Spacer(),
-              Text(
-                'From',
-                style: labelStyle,
-              ),
-              DropdownButton<String>(
-                value: _startMeasure,
-                items: _measures.map((String _measuresValue) {
-                  return DropdownMenuItem(
-                    value: _measuresValue,
-                    child: Text(
-                      _measuresValue,
-                      style: inputStyle,
-                    ),
-                  );
-                }).toList(),
-                onChanged: (String newValue) {
-                  setState(() {
-                    _startMeasure = newValue;
-                  });
-                },
-              ),
-              Spacer(),
-              Text(
-                'To',
-                style: labelStyle,
-              ),
-              Spacer(),
-              DropdownButton(
-                items: _measures.map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(
-                      value,
-                      style: inputStyle,
-                    ),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  setState(() {
-                    _convertedMeasure = value;
-                  });
-                },
-                value: _convertedMeasure,
-              ),
-              Spacer(flex: 2),
+              SizedBox(height: 30),
               ElevatedButton(
-                onPressed: () => true,
+                onPressed: () {
+                  if (_startMeasure.isEmpty ||
+                      _convertedMeasure.isEmpty ||
+                      _numberFrom == 0) {
+                    return;
+                  } else {
+                    convert(_numberFrom, _startMeasure, _convertedMeasure);
+                  }
+                },
                 child: Text(
                   'Convert',
                   style: inputStyle,
                 ),
               ),
-              Spacer(),
+              SizedBox(height: 30),
               Text(
-                (_numberFrom == null) ? '' : _numberFrom.toString(),
+                (_resultMessage == null) ? '' : _resultMessage,
                 style: labelStyle,
               ),
-              Spacer(),
             ],
           ),
         ),
